2d7621596e81cb9cde65f3cff2f9602cfef6699f
khushik8605 <142583464+khushik8605@users.noreply.github.com>
2025-02-01 23:09:45 +0530

    Added Array-Based Statistical Functions

--- Calculator.java
+++ Calculator.java
@@ -11,3 +11,24 @@ public int fibonacci(int n) {
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+public double sumArray(double[] array) {
+    double sum = 0;
+    for (double num : array) sum += num;
+    return sum;
+}
+
+public double mean(double[] array) {
+    return sumArray(array) / array.length;
+}
+
+public double variance(double[] array) {
+    double mean = mean(array);
+    double sum = 0;
+    for (double num : array) sum += Math.pow(num - mean, 2);
+    return sum / array.length;
+}
+
+public double standardDeviation(double[] array) {
+    return Math.sqrt(variance(array));
+}
+
